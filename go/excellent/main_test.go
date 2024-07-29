1abc808254d8c2b6e0e550d3854f55fc11f29d74
shinobu-takahata <shinoshino2335@gmail.com>
2024-07-29 10:49:43 +0900

    テストコード修正

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -1,7 +1,7 @@
 package main
 import "testing"
 func TestEvenOrOdd(t *testing.T) {
- result := EvenOrOdd(5)
+ result := EvenOrOdd(10)
  if result != "even" {
  t.Errorf("expected: even, actual : %s", result)
  }
